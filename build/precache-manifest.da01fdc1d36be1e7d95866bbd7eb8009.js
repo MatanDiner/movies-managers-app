125525f66972e7f80feb227a43ad519a4e821e4a
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-03-30 20:48:41 +0300

    Add files via upload

---
+++ build/precache-manifest.da01fdc1d36be1e7d95866bbd7eb8009.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "babe049820ae617d5aaeb446a90def79",
+    "url": "/static/media/Robin Hood.babe0498.jpg"
+  },
+  {
+    "revision": "01ae8621a7ac245d0e67",
+    "url": "/static/css/main.32a9a051.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d71ed1b7cf73bbb64b6",
+    "url": "/static/js/2.fd7936f6.chunk.js"
+  },
+  {
+    "revision": "305f9d62dbaccb39f4975f58cc15aa8e",
+    "url": "/static/media/Aladdin.305f9d62.jpg"
+  },
+  {
+    "revision": "5e84dae0e874818768259a2d91b40392",
+    "url": "/static/media/Antman.5e84dae0.jpg"
+  },
+  {
+    "revision": "c54efa7be87aa81b75f199025377e5bc",
+    "url": "/static/media/Aquaman.c54efa7b.jpg"
+  },
+  {
+    "revision": "4abeff16fdfb5df7b9e1f6d0aadb01a4",
+    "url": "/static/media/Cinderella.4abeff16.jpg"
+  },
+  {
+    "revision": "d58afc7fd67cf5828ef0a5f4d1fe22f1",
+    "url": "/static/media/Dog Day Afternoon.d58afc7f.jpg"
+  },
+  {
+    "revision": "6fc2b2e8687af84b52932962ea0971e2",
+    "url": "/static/media/Harry Potter.6fc2b2e8.jpg"
+  },
+  {
+    "revision": "d667e3d609ca9b5561861de63d2f3cfd",
+    "url": "/static/media/Lord of the Rings.d667e3d6.jpg"
+  },
+  {
+    "revision": "110247dc828dd32a229c65d5136b74ef",
+    "url": "/static/media/Peter Pan.110247dc.jpg"
+  },
+  {
+    "revision": "01ae8621a7ac245d0e67",
+    "url": "/static/js/main.ba9bee70.chunk.js"
+  },
+  {
+    "revision": "e64788e095b5cd7172bb2acb28902dd5",
+    "url": "/static/media/Snow White.e64788e0.jpg"
+  },
+  {
+    "revision": "4deb14d7dcca64065d07ace9a327bdcc",
+    "url": "/static/media/The Lion King.4deb14d7.jpg"
+  },
+  {
+    "revision": "cf7983a9ddcf49ea54e95832ebe0a37f",
+    "url": "/static/media/The Little Mermaid.cf7983a9.jpg"
+  },
+  {
+    "revision": "a0c2f81f219b720417d0d6d560410de1",
+    "url": "/static/media/Thor.a0c2f81f.jpg"
+  },
+  {
+    "revision": "55cdba1a123b3a1417ff8b082b8cbe50",
+    "url": "/static/media/Titanic.55cdba1a.jpg"
+  },
+  {
+    "revision": "d0b3d3a195a7bce6660e95acc47621ba",
+    "url": "/static/media/Troy.d0b3d3a1.jpg"
+  },
+  {
+    "revision": "53cbe013fe07a4b0f5eb1841532eeb0e",
+    "url": "/static/media/Venom.53cbe013.jpg"
+  },
+  {
+    "revision": "e23ffc3dabf2ede086aa4c8c37cbe10d",
+    "url": "/static/media/batman.e23ffc3d.jpg"
+  },
+  {
+    "revision": "2aafe54f73f725948544c931efffcdc9",
+    "url": "/static/media/spiderman.2aafe54f.jpg"
+  },
+  {
+    "revision": "a97ca235d7c7292ce8bbe02734e6c5d3",
+    "url": "/static/media/xman.a97ca235.jpg"
+  },
+  {
+    "revision": "bb6978463d3c8683ef9af5e13b8bc09a",
+    "url": "/index.html"
+  }
+];
